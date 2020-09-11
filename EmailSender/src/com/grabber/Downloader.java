b6d83b47ca094aab7d287f572eaee912f4358290
qdH3ErcZg8ZehCdf <MDf4nBNjqrufGnxS@protonmail.com>
2020-09-11 14:18:53 +0300

    V3 Downloader RegExp

--- EmailSender/src/com/grabber/Downloader.java
+++ EmailSender/src/com/grabber/Downloader.java
@@ -1,18 +1,43 @@
 package com.grabber;
 
-
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Downloader {
 
-	public static void main(String[] args) throws Exception {
+	static Set<String> urls = new HashSet<String>();
+	static int count =0;
 	
+	
+	public static void main(String[] args) throws Exception {
+		
+		Pattern p = Pattern.compile( "(?:^|[\\W])((ht|f)tp(s?):\\/\\/|www\\.)"
+                + "(([\\w\\-]+\\.){1,}?([\\w\\-.~]+\\/?)*"
+                + "[\\p{Alnum}.,%_=?&#\\-+()\\[\\]\\*$~@!:/{};']*)",Pattern.CASE_INSENSITIVE);
+
 		String response = "";
 		String url = "https://cybereasy.site";
 //		-----------------
 		
 		response = c.getHTML(url);
-		System.out.println(response);
+//		System.out.println(response);
 		
+		
+		
+        Matcher m = p.matcher(response);
+        while (m.find()) {
+            for (int i = 1; i <= m.groupCount(); i++) {
+                urls.add(m.group(i));
+                count ++;	
+            }
+        }
+
+        System.out.println(count);
+        for (String u: urls) {
+        	System.out.println(u);
+        }
 		
 		
 		
