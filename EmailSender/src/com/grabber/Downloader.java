18709a96223780fc0d5dfbfb8e04f3f8945aa261
qdH3ErcZg8ZehCdf <MDf4nBNjqrufGnxS@protonmail.com>
2020-09-11 13:58:49 +0300

    v2 with Downloader

---
+++ EmailSender/src/com/grabber/Downloader.java
@@ -0,0 +1,24 @@
+package com.grabber;
+
+
+
+public class Downloader {
+
+	public static void main(String[] args) throws Exception {
+	
+		String response = "";
+		String url = "https://cybereasy.site";
+//		-----------------
+		
+		response = c.getHTML(url);
+		System.out.println(response);
+		
+		
+		
+		
+		
+		
+		
+//	---------------------------
+	}
+}
